38243f1bc44907cd036c9f6e328c24a941e5a359
Sanna Edström <sannaedstrom@gmail.com>
2020-08-28 13:08:36 +0200

    Progress number 01

--- csharp Project 2020/Session03/Session03exercis01/Session03Execises02/Program.cs
+++ csharp Project 2020/Session03/Session03exercis01/Session03Execises02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Session03Execises02
 {
@@ -10,11 +11,47 @@ namespace Session03Execises02
             var input = Console.ReadLine();
 
             var inputArray = input.Split(",", StringSplitOptions.RemoveEmptyEntries);
+            double[] numberArray = new double[inputArray.Length];
 
-            foreach (var number in inputArray)
+            for (int i = 0; i < inputArray.Length; i++)
+
             {
-                Console.WriteLine("Värdet är " + number);
+                try
+                {
+                    numberArray[i] = Convert.ToDouble(inputArray[i]);
+                }
+                catch (Exception)
+                {
+                    numberArray[1] = 0;
+                }
+
+                foreach (var number in inputArray)
+                {
+                    Console.WriteLine("Värdet" + number.ToString());
+                }
+                
+                int max = Int32.MinValue; 
+                int min = Int32.MaxValue;
+
+                foreach (string number in inputArray)
+                {
+                    int currentNumber = int.Parse(number); 
+
+                    if (currentNumber > max)  
+                        max = currentNumber;
+
+                    if (currentNumber < min)
+                        min = currentNumber;
+
+              
+                }
+                Console.WriteLine(string.Format("Minsta värdet: {0} Högsta värdet: {1}", min, max));
+                
+
+                Console.ReadKey();
             }
+
         }
     }
+    
 }
