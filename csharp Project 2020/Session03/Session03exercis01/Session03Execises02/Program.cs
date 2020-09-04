790a95dad7ab039dec6ce49c4f05ce9dc8be5db5
Sanna Edström <sannaedstrom@gmail.com>
2020-09-04 12:42:18 +0200

    Progress number 02

--- csharp Project 2020/Session03/Session03exercis01/Session03Execises02/Program.cs
+++ csharp Project 2020/Session03/Session03exercis01/Session03Execises02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Session03Execises02
@@ -12,46 +13,51 @@ namespace Session03Execises02
 
             var inputArray = input.Split(",", StringSplitOptions.RemoveEmptyEntries);
             double[] numberArray = new double[inputArray.Length];
+            double total = 0;
+            double count = inputArray.Length;
+            double middle = 0;
+            int max = Int32.MinValue;
+            int min = Int32.MaxValue;
 
             for (int i = 0; i < inputArray.Length; i++)
-
             {
+                bool test;
+        
                 try
                 {
                     numberArray[i] = Convert.ToDouble(inputArray[i]);
+                    test = true;
                 }
                 catch (Exception)
                 {
                     numberArray[1] = 0;
+                    test = false;
+                }
+
+                if (test == false)
+                {
+                    continue;
+                    
                 }
 
                 foreach (var number in inputArray)
                 {
-                    Console.WriteLine("Värdet" + number.ToString());
-                }
-                
-                int max = Int32.MinValue; 
-                int min = Int32.MaxValue;
-
-                foreach (string number in inputArray)
-                {
-                    int currentNumber = int.Parse(number); 
-
-                    if (currentNumber > max)  
+                    int currentNumber = int.Parse(number);
+                    if (currentNumber > max)
                         max = currentNumber;
 
                     if (currentNumber < min)
                         min = currentNumber;
 
-              
+                    total += currentNumber;
+                    middle = total / count;
+
+                    Console.WriteLine("Värdet: " + number.ToString());
                 }
-                Console.WriteLine(string.Format("Minsta värdet: {0} Högsta värdet: {1}", min, max));
-                
+                    Console.WriteLine(string.Format("Minsta värdet: {0} Högsta värdet: {1} Medelvärdet: {2}", min, max, middle));
 
-                Console.ReadKey();
-            }
-
+                    Console.ReadKey();
+            } 
         }
-    }
-    
-}
+    }  
+} 
